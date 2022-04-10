57f32230c3a98dbd0c8769d482d9be445ff07396
unknown <harshilpatel982002@gmail.com>
2022-04-10 17:24:33 +0530

    Updates

---
+++ precache-manifest.d70a714f90ea259a19182a424bf6c1b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239240c49bcd4b36af7153ff8b63421b",
+    "url": "/quizy/index.html"
+  },
+  {
+    "revision": "9e9a90f0d497d92b3f99",
+    "url": "/quizy/static/css/main.35088b67.chunk.css"
+  },
+  {
+    "revision": "49cea2de3c30b546531a",
+    "url": "/quizy/static/js/2.1cebe0c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizy/static/js/2.1cebe0c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e9a90f0d497d92b3f99",
+    "url": "/quizy/static/js/main.d4c212e3.chunk.js"
+  },
+  {
+    "revision": "36be5a5a6ad99540eff5",
+    "url": "/quizy/static/js/runtime-main.b23a6621.js"
+  }
+]);
